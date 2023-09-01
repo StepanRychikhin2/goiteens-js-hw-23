32fccbf8afbf9a6de428a286aa3f5bfdf4e3db6f
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-09-01 18:38:34 +0300

    hw-23-to worcs

---
+++ js/task-1_1.js
@@ -0,0 +1,28 @@
+// class Account {
+//     constructor ({ login, email }) {
+//     this.logins = login;
+//     this.emails = email;
+//     }
+    
+    
+    
+//     getInfo () {
+//         console.log(this.logins);
+//         console.log(this.emails);
+// }
+// }
+
+// // function
+// const mango = new Account({ 
+//     login: "Mangozedog",
+//      email: "mango@dog.woof"
+//     });
+
+// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+// const poly = new Account({
+//   login: "Poly",
+//   email: "poly@mail.com",
+// });
+// // console.log(poly);
+// poly.getInfo(); // Login: Poly, Email: poly@mail.com
---
+++ js/task-2_2.js
@@ -0,0 +1,33 @@
+// class User  {
+//   constructor({ name, age, followers }) {
+//     this.nameUser = name;
+//     this.ageUser = age;
+//     this.followersUser = followers;
+//   }
+  
+
+//   getInfo(nameUser, ageUser, followersUser) {
+//     console.log(`User ${this.nameUser} is ${this.ageUser} years old and has ${this.followersUser} followers`);
+//   }
+// };
+
+// function greetGuest(greeting) {
+//   // console.log(`${greeting}, ${this.username}.`);
+//   console.log(
+//     `User ${this.User.name} is ${this.User.age} years old and has ${this.User.followers} followers`
+//   );
+// }
+
+
+// const mango = new User({
+//   name: "Mango",
+//   age: 2,
+//   followers: 20,
+// });
+// mango.getInfo(); // User Mango is 2 years old and has 20 followers
+// const poly = new User({
+//   name: "Poly",
+//   age: 3,
+//   followers: 17,
+// });
+// poly.getInfo(); // User Poly is 3 years old and has 17 followers
